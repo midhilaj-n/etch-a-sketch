a5a69680819103dc597acf125532d14c04c247a3
midhilaj-n <midhilajnisam1@gmail.com>
2024-10-05 20:09:54 +0530

    Added functionality for user can decide the grid size

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
-const container = document.querySelector('.container');
+const input = prompt("Enter your grid size(1-100): ");
+const gridSize = parseInt(input);
 
-for (let i = 0; i < 256; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('item');
-    container.appendChild(gridItem);
+
+
+generateGrid(gridSize);
+
+
+
+
+function generateGrid(sizeOfGrid){
+
+  
+
+    const container = document.querySelector('.container');
+
+    if (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
+        alert("Please enter a valid grid size between 1 and 100.");
+        return; 
+    }
+
+    container.innerHTML = '';
+
+    const itemSize = `calc(100% / ${gridSize})`;
+
+    const totalItems = sizeOfGrid * sizeOfGrid;
     
+    for (let i = 0; i < totalItems; i++) {
+    
+
+   
+        const gridItem = document.createElement('div');
+
+        gridItem.classList.add('item');
+
+        gridItem.style.width = itemSize;
+
+        gridItem.style.height = itemSize;
+
+        container.appendChild(gridItem);
+
+        gridItem.addEventListener('mouseenter', function() {
+            const randomColor = getRandomColor()
+            this.style.backgroundColor = randomColor; 
+        });
+
+        function getRandomColor() {
+            const r = Math.floor(Math.random() * 256); 
+            const g = Math.floor(Math.random() * 256); 
+            const b = Math.floor(Math.random() * 256); 
+            return `rgb(${r}, ${g}, ${b})`; 
+        }
+        
+    }
 }
